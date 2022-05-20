91f6b0b2e91b6dbf1ce3b9630b81e6e0714c7005
Superzzp <zhangzhipian1@gmail.com>
2022-05-20 00:34:47 -0700

    update cors origin

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use('/openai', require('./routes/openai.js'))
 
 // Enable cors
-app.use(cors())
+app.use(cors({origin: ['https://funwithopenai.herokuapp.com']}))
 
 app.get('/', async (req, res) => {
     res.status(200).json("Service is running")
