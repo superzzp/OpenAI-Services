76be808ca659c59015e77deea65ceb2a216d1b59
zhipian <zhangzhipian1@gmail.com>
2023-02-27 23:59:45 -0800

    add routes for ai image generation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 
 const app = express()
 
@@ -19,13 +19,16 @@ app.set('trust proxy', 1)
 app.use(express.json())
 
 // Enable cors
-app.use(cors({origin: ['https://funwithopenai.herokuapp.com']}))
+app.use(cors({origin: [
+    'https://funwithopenai.herokuapp.com',
+    'http://localhost:3000'
+]}))
 
 // Routes
 app.use('/openai', require('./routes/openai.js'))
 
 app.get('/', async (req, res) => {
-    res.status(200).json("Service is running")
+    res.status(200).json("OpenAI Proxy Service is running")
 })
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT))
--- routes/openai.js
+++ routes/openai.js
@@ -6,20 +6,21 @@ const router = express.Router()
 const OPAI_BASE_URL = process.env.OPAI_BASE_URL
 const OPAI_CREDENTIALS = process.env.OPAI_CREDENTIALS
 
-router.post('/', async (req, res) => {
+router.post('/completions', async (req, res) => {
     try {
         const body = req.body;
-        if (body.prompt == undefined) {
+        if (body.prompt == undefined || body.prompt.trim().length == 0) {
             res.status(401).json('request missing prompt')
         } else if (body.max_tokens == undefined) {
             res.status(402).json('request missing max_tokens')
         } else if (body.max_tokens != undefined && body.max_tokens > 512) {
-            res.status(403).json('Can\'t submit a request for more than 512 words response')
+            res.status(403).json('Can\'t request for more than 512 words response')
         } else if (body.model == undefined) {
             res.status(405).json('request missing data model')
         } else {
-            const url = OPAI_BASE_URL + body.model + '/completions';
+            const url = OPAI_BASE_URL + '/completions';
             const apiBody = {
+                "model": body.model,
                 "prompt": body.prompt,
                 "max_tokens": body.max_tokens,
                 "temperature": body.temperature,
@@ -34,6 +35,33 @@ router.post('/', async (req, res) => {
             const apiRes = await needle('post', url, apiBody, options)
             const apiResText = apiRes.body.choices[0].text;
             res.status(200).json({"text": apiResText})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }
+})
+
+router.post('/generations', async (req, res) => {
+    try {
+        const body = req.body;
+        if (body.prompt == undefined) {
+            res.status(401).json('request missing prompt')
+        } else {
+            const url = OPAI_BASE_URL + '/images/generations';
+            const apiBody = {
+                "prompt": body.prompt,
+                "n": 1,
+                "size": "1024x1024"
+            }
+            var options = {
+                headers: { 'Content-Type': 'application/json',
+                'Authorization': OPAI_CREDENTIALS }
+            }
+            const apiRes = await needle('post', url, apiBody, options)
+            const apiResURL = apiRes.body.data[0].url
+            print(apiResURL)
+            res.status(200).json({"url": apiResURL})
         }
     } catch (error) {
         console.log(error)
